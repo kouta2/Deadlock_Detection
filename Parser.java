6431c371ec160c87d2bcc898b08da4f8a86cd825
Drake <ptdrake2@sp17-cs425-g32-05.cs.illinois.edu>
2017-04-26 18:17:48 -0500

    Parser finished

--- Parser.java
+++ Parser.java
@@ -12,24 +12,28 @@ public class Parser
 	static HashMap<String, String> local; 
 
 
-	private static void re_init();  
+	private static void re_init(){
+		local = new HashMap<String,String>(); 
+	}  
 
 
     public static void handle_one_line(String cmd){
     
 		RPCFunctions r = Main.rpc_connect.get_connection(10);
 		
-		
-   
 		if (cmd.toUpperCase().equals("BEGIN")){
-			r.c_abort(Main.PROCESS_ID); //This machine is resetting its transaction
+			try{	
+				r.c_abort(Main.PROCESS_ID); //This machine is resetting its transaction
+			}catch (Exception e){}
 			re_init(); 
 			System.out.println("OK");
 		}
 
 		if (cmd.toUpperCase().equals("ABORT")){
 			//clear out local key value store
-			r.c_abort(Main.PROCESS_ID); 
+			try{
+				r.c_abort(Main.PROCESS_ID); 
+			}catch (Exception e){}; 
 			re_init(); 
 			System.out.println("ABORT");
 		}
@@ -37,7 +41,11 @@ public class Parser
 		if (cmd.toUpperCase().equals("COMMIT")){
 			//TODO: implement actual committing
 			//clear key value store
-			r.c_commit(local);
+
+			try{
+				r.c_commit(Main.PROCESS_ID, local);
+			}catch(Exception e){} 
+
 			re_init();  
 			System.out.println("COMMIT OK");
 		}
@@ -50,28 +58,50 @@ public class Parser
 		String key = args[1];
 
 	 
-		String result;
+		String result = null; 
 	
 		if (arg.toUpperCase().equals("GET")){
 			result = local.get(key); 
-			local = result != null; 
-			r.c_get(machine,key,local,result); 
+			boolean local_flag = (result != null); 
 			
-
-
+			try{
+				result = r.c_get(Main.PROCESS_ID,key,local_flag,result); 
+			}catch (Exception e){} 
+			
+			if (result == null){	
+				System.out.println("ABORT"); 
+				
+				try{
+					r.c_abort(Main.PROCESS_ID); 
+				}catch(Exception e){} 
+				re_init(); 
+			}else if (result.equals("NOT FOUND")){
+				try{
+					r.c_abort(Main.PROCESS_ID); 
+				}catch (Exception e){}
+				re_init(); 
+				System.out.println(result); 
+			}else{
+				System.out.println(result); 
+			}
+			
 		}
 
 		if (arg.toUpperCase().equals("SET")){
 			String value = args[2]; 
-			result = r.s_set(key, value);
+			try{
+				result = r.c_set(Main.PROCESS_ID, key, value);
+			}catch (Exception e){}
+			if (result == null){
+				try{
+					r.c_abort(Main.PROCESS_ID); 
+				}catch (Exception e){}
+				re_init(); 
+				System.out.println("ABORT"); 	
+			}else{
+				local.put(key, value); 
+				System.out.println(result); 
+			}
 		}
-		
-
-		System.out.println(result); 
-
-
-
-
-    }
-
+	}
 }
