04b0947a5bc561ea1c5a9bfa4e0456ddd20be298
Drake <ptdrake2@sp17-cs425-g32-05.cs.illinois.edu>
2017-04-26 14:25:39 -0500

    Parser

--- Main.java
+++ Main.java
@@ -8,32 +8,51 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.net.InetAddress;
-import java.util.Scanner;
+iimport java.util.Scanner;
 import java.util.ArrayList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.HashMap;
 
 public class Main implements RPCFunctions
 {
     static int PORT_NUM = 2002;
     static String SERVER_ID = "";
     static ConnectToOtherRPCs rpc_connect;
+	static HashMap<String,String> kv;
+	static HashMap<String, Integer> machines;
 
     public String set(String key, String value)
     {
-        // TODO: localy update data structure and return proper info
-        return null;
+       	kv.put(key,value);
+		return null; 
     }
 
     public String get(String key)
     {
-        // TODO: locally grab data and return proper info
-        return null;
+        return kv.get(key); 
     }
+
+	private void init(){
+		machines = new HashMap<String,Integer>;
+		machines.put("A",1); 
+		machines.put("B",2); 
+		machines.put("C",3); 
+		machines.put("D",4); 
+		machines.put("E",5); 
+
+		
+
+	}
+
 
     public static void main(String[] args)
     {
-        try 
+     	
+	
+		init();//init stuff 
+
+		try 
         { 
             int process_num = Integer.parseInt(InetAddress.getLocalHost().getHostName().substring(15, 17)); 
             String[] server_id = {"A", "B", "C", "D", "E"};
--- Parser.java
+++ Parser.java
@@ -1,3 +1,6 @@
+import java.util.HashMap
+
+
 /*
  *
  * Parses stdin
@@ -5,9 +8,58 @@
 public class Parser
 {
 
-    public static void handle_one_line(String cmd)
-    {
-        // TODO: implement one client command
+
+	
+
+
+    public static void handle_one_line(String cmd){
+    
+		RPCFunctions r = Main.rpc_connect.get_connection(10);
+		
+		
+   
+		if (cmd.toUpperCase().equals("BEGIN")){
+			//TODO: implement locking/whatever
+			System.out.println("OK");
+		}
+
+		if (cmd.toUpperCase().equals("ABORT")){
+			//TODO: implement rollback
+			System.out.println("ABORT");
+		}
+
+		if (cmd.toUpperCase().equals("COMMIT")){
+			//TODO: implement actual committing
+			System.out.println("COMMIT OK");
+		}
+		
+		String[] args = cmd.split(" ");
+		
+		String arg = args[0];
+		String[] obj = args[1].split(".");
+		int machine = Main.machines.get(obj[0]);
+		String key = obj[1];
+
+		
+	 
+		RPCFunctions r = Main.rpc_connect.get_connection(machine); 
+		String result;
+	
+		if (arg.toUpperCase().equals("GET")){
+			result = r.set(key); 		
+		}
+
+		if (arg.toUpperCase().equals("SET")){
+			String value = args[2]; 
+			result = r.set(key, value);
+		}
+		
+
+		System.out.println(result); 
+
+
+
+
     }
 
 }
