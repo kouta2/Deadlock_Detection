e228b6f67012dfb45e67def2a83abea5482e8b28
kouta2 <arvind.kouta@gmail.com>
2017-05-16 00:11:58 -0500

    Removed class files directory, renamed design document, moved testing material into a test folder

--- script.py
+++
@@ -1,5 +0,0 @@
-print('client.operation(\'BEGIN\', expected=\'OK\')')
-for i in range(1000):
-    print('client.operation(\'SET A.' + str(i) + ' = ' + str(i) + '\', expected=\'OK\')')
-
-print('client.operation(\'COMMIT\', expected=\'COMMIT OK\')')
